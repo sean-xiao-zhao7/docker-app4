0c0260d0333d9b9e66340e7b7ec976601626cedf
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2023-03-31 13:49:08 -0400

    Add nodejs server functionalities

--- server/index.js
+++ server/index.js
@@ -20,9 +20,42 @@ const pgClient = new Pool({
 pgClient.on("error", (error) => {
     console.log(error);
 });
+pgClient
+    .query("CREATE TABLE IF NOT EXISTS values (number INT)")
+    .catch((err) => console.log(err));
 
-// const redisClient = redis.createClient({
-//     host: keys.redisHost,
-//     port: keys.redisPort,
-//     retry_strategy: () => 1000,
-// });
+// redis
+const redisClient = redis.createClient({
+    host: keys.redisHost,
+    port: keys.redisPort,
+    retry_strategy: () => 1000,
+});
+const redisPublisher = redisClient.duplicate();
+
+// Express router
+app.get("/", (req, res) => {
+    return res.send("Home");
+});
+app.get("/values/all", async (req, res) => {
+    const values = await pgClient.query("SELECT * FROM values;");
+    return res.send(values.rows);
+});
+app.get("/values/current", async (req, res) => {
+    redisClient.hgetall("values", (err, values) => {
+        return res.send(values);
+    });
+});
+app.post("/values/new", async (req, res) => {
+    const index = req.body.index;
+    if (+index > 40) {
+        return res.status(422).send("Index larger than 40.");
+    } else {
+        redisClient.hset("values", index, "Placeholder");
+        redisPublisher.publish("insert", index);
+        pgClient.query("INSERT INTO values(number) VALUES($1)", [index]);
+        return res.send("Calculating Fibonacci.");
+    }
+});
+app.listen(5000, (err) => {
+    console.log("Listening on 5000.");
+});
